e44f4577dabe6b4a31b52816c596e229218cad75
Garrett Trott <garretttrott@gmail.com>
2021-02-28 04:28:21 -0800

    Add comments

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ app.get("/greetings", (req, res)=> {
   res.json({greeting: "Hello World"})
 })
 
+// Send a GET request to /quotes READ a list of quotes 
+
+// Send a GET request to /quotes/:id READ(view) a quote
+
+// Send a POST Request to /quotes CREATE a new quote 
+
+// Send a PUT request to /quotes/:id UPDATE (edit) a quote
+
+// Send a DElETE request /quotes/:id to DELETE a quote
+
+// Send a GET request to /quotes/quote/random READ (view) a random quote
+
+
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
 
 
